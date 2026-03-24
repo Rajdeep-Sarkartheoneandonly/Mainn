4be2af51714e73482878ff19b51014136f921bb6
Rajdeep-Sarkartheoneandonly <rajdeepsarkar2007@gmail.com>
2026-03-24 19:56:27 +0530

    sss

---
+++ c.py
@@ -0,0 +1,9 @@
+import time
+print('[__________]',end = '\r')
+time.sleep(2)
+print('[###_______]',end = '\r')
+time.sleep(2)
+print('[########__]',end = '\r')
+time.sleep(2)
+print('[##########]')
+print('Loading complete!')
