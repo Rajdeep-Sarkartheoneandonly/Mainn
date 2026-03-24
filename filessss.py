c1c268d5f6b9bacfc0c5ef8e8c58dffc7a3f0ba3
Rajdeep-Sarkartheoneandonly <rajdeepsarkar2007@gmail.com>
2026-03-24 18:01:14 +0530

    as

--- filessss.py
+++ filessss.py
@@ -1,8 +1,7 @@
-while True:
-    f = input()
-    try:
-        with open(f+'.txt', 'r') as file:
-            content = file.read()
-            print(content)
-    except:
-        print("File not found.")
+import csv
+with open('w.csv','r') as f2:
+    rd = csv.reader(f2, delimiter = '|')
+    for i in rd:
+        for m in i:
+            print(m+'\n')
+        print()
