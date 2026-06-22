f358ea9ab1a0745d687df4a56f759289299a44f6
Rajdeep-Sarkartheoneandonly <rajdeepsarkar2007@gmail.com>
2026-06-22 22:02:49 +0530

    division explicit conversion

--- myfirstprogram.c
+++ myfirstprogram.c
@@ -4,10 +4,12 @@ int main() {
     int mynum = 15;
     float myfloat = 5.99;
     char myletter = 'D';
+    float value = (float) mynum/myfloat;
     printf("Hello World!\n");
     printf("%d\n", mynum);
     printf("%f\n", myfloat);
     printf("%c\n", myletter);
+    printf("%f\n", value);
     printf("%d, %f, %c\n", mynum, myfloat, myletter);
     return 0;
 }
