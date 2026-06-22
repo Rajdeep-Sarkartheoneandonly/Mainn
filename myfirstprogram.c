c239126f3a4535f15c03d1df6e437550223f09da
Rajdeep-Sarkartheoneandonly <rajdeepsarkar2007@gmail.com>
2026-06-22 20:39:58 +0530

    Your commit message

---
+++ myfirstprogram.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int mynum = 15;
+    float myfloat = 5.99;
+    char myletter = 'D';
+    printf("Hello World!\n");
+    printf("%d\n", mynum);
+    printf("%f\n", myfloat);
+    printf("%c\n", myletter);
+    printf("%d, %f, %c\n", mynum, myfloat, myletter);
+    return 0;
+}
