9528cb25cadc86729f4189edc7b46096c760c83f
Rajdeep-Sarkartheoneandonly <rajdeepsarkar2007@gmail.com>
2026-06-23 11:15:41 +0530

    modified voting age programme with shorthand

--- voteage.c
+++ voteage.c
@@ -3,6 +3,6 @@
 int main() {
     int myage = 18;
     int voteage = 18;
-    (myage >= voteage) ? printf("Old enough to vote") : printf("Not old enough to vote");
+    (myage >= voteage) ? printf("Old enough to vote\n") : printf("Not old enough to vote\n");
     return 0;
 }
