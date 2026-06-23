46668d71cc643f359ed328c83c7919cc78950d78
Rajdeep-Sarkartheoneandonly <rajdeepsarkar2007@gmail.com>
2026-06-23 11:01:19 +0530

    voting age programe

---
+++ voteage.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int myage = 18;
+    int voteage = 18;
+    if (myage >= voteage) {
+        printf("Old enough to vote\n");
+    } else {
+        printf("Not old enough to vote\n");
+    }
+}
