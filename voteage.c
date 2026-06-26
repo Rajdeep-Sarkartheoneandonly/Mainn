46b6cf798f1250b3a9175efb67e207f8c0f4fe16
Rajdeep-Sarkartheoneandonly <rajdeepsarkar2007@gmail.com>
2026-06-26 10:57:26 +0530

    modified voteage.c to use user input

--- voteage.c
+++ voteage.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int myage = 18;
+    int myage;
     int voteage = 18;
+    printf("Enter your age: ");
+    scanf("%d", &myage);
     (myage >= voteage) ? printf("Old enough to vote\n") : printf("Not old enough to vote\n");
     return 0;
 }
