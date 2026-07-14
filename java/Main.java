808af4ea446a97654916e9580a350d562cedc406
Rajdeep-Sarkartheoneandonly <rajdeepsarkar2007@gmail.com>
2026-07-14 14:32:48 +0530

    Started learning java and moved to Kate

---
+++ java/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int studentID = 12345;
+        double score = 95.5;
+        char grade = 'A';
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Score: " + score);
+        System.out.println("Grade: " + grade);
+        int x = 10;
+        String str =
+            x > 4 && x < 8
+                ? "x less than 8 and more than 4"
+                : x > 8
+                  ? "x greater than 8"
+                  : "x less than 4";
+        System.out.println(str);
+        String line = "Hello everyone!";
+        System.out.println(line);
+        String[] words = line.split(" ");
+        System.out.println(words.length);
+        for (String word : words) {
+            System.out.print(word + " ");
+        }
+        Second myobj = new Second();
+        System.out.println(myobj.x + myobj.y);
+    }
+}
